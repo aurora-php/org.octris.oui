e59ae04f062d30a8bc68d970707bb97c913712ff
Harald Lapp <harald.lapp@gmail.com>
2011-10-29 19:30:00 +0200

    initial commit oui dom class

---
+++ libsjs/core/oui.dom.js
@@ -0,0 +1,15 @@
+/**
+ * DOM processing library.
+ *
+ * @octdoc      c:oui/dom
+ * @copyright   copyright (c) 2011 by Harald Lapp
+ * @author      Harald Lapp <harald@octris.org>
+ */
+/**/
+
+;(function() {
+    if ('dom' in oui) return;
+
+    oui.dom = {
+    }
+})();
