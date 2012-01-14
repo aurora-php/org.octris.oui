cd3132a264e599616f72b7643fc871eacde3a4fd
Harald Lapp <harald.lapp@gmail.com>
2012-01-14 01:49:17 +0100

    implemented DOM helper

--- libsjs/oui/oui.js
+++ libsjs/oui/oui.js
@@ -1,51 +1,109 @@
-;(function(window) {
-	if ('oui' in window) return;
-	
-	var oui = {};
-	oui.$ = jQuery.noConflict();
+/* */
 
-	window.oui = {};
-	
-	// core functionality
-	oui.create = function(tag, def) {
-		def = def || {};
-		
+;(function(window) {
+    if ('oui' in window) return;
+
+    var oui = {};
+    oui.$ = jQuery.noConflict();
+
+    window.oui = oui;
+
+    // core functionality
+
+    /**
+     * Create DOM construct.
+     *
+     * @octdoc      oui/create
+     * @param       string          tag             Name of tag to create.
+     * @param       object          def             Definition of DOM construct to create.
+     * @return      DOMElement                      Created DOM Node.
+     */
+    oui.create = function(tag, def) {
+        def = def || {};
+
         function _get_tag(def) {
             var tag = '';
-                
+
             if (typeof def == 'object') {
                 for (var i in def) {
                     tag = i;
                     break;
                 }
             }
-                
+
             return tag;
         }
-            
+
+        function _set_properties(node, def) {
+            var trigger = null;
+            var name, evt;
+
+            for (var attr in def) {
+                console.log(attr, def);
+
+                switch (attr) {
+                    case 'children':
+                        continue;
+                    case '#trigger':
+                        if (typeof def[attr] == 'function') {
+                            trigger = def[attr];
+                        }
+                        break;
+                    case '#html':
+                        node.append(def[attr]);
+                        break;
+                    case '#text':
+                        node.append(document.createTextNode(def[attr]));
+                        break;
+                    case '#data':
+                        for (name in def[attr]) {
+                            node.data(name, def[attr][name]);
+                        }
+                        break;
+                    case 'class':
+                    case 'className':
+                        node.attr('className', def[attr]);
+                        break;
+                    case 'styles':
+                    case 'css':
+                        node.css(def[attr]);
+                        break;
+                    default:
+                        if (attr.substr(0, 2) == 'on') {
+                            evt = attr.substr(2);
+
+                            if (typeof def[attr] == 'string') {
+                                node.bind(evt, (function(js) {
+                                    var cb = function() {
+                                        eval(js);
+                                    };
+
+                                    cb.apply(node);
+                                })(def[attr]));
+                            } else if (typeof def[attr] == 'function') {
+                                node.bind(evt, def[attr]);
+                            }
+                        } else {
+                            node.attr(attr, def[attr]);
+                        }
+                }
+            }
+
+            if (trigger != null) {
+                trigger(node);
+            }
+        }
+
         function _build(tag, def) {
             var i, len, tmp;
 
-			// create new node
-			var node = oui.$(document.createElement(tag));
-			
-			// apply attributes to node
-			for (i in def) {
-				switch (i) {
-				case 'children':
-					continue;
-				case '...':
-					...
-					node.attr(i, ...);
-					
-				}
-			}
-                
+            var node = oui.$(document.createElement(tag));
+            _set_properties(node, def);
+
             if ('children' in def && def['children'] instanceof Array) {
-                // iterate over children
                 for (i = 0, len = def['children'].length; i < len; ++i) {
                     if ((tmp = _get_tag(def['children'][i])) !== '') {
-						node.add(_build(tmp, def['children'][i][tmp]));
+                        node.append(_build(tmp, def['children'][i][tmp]));
                     }
                 }
             }
@@ -53,6 +111,7 @@
             return node;
         }
 
-		return _build(tag, def);
-	}
+        return _build(tag, def);
+    }
 })(window);
+
