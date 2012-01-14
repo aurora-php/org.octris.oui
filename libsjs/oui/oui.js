e27e6a3686b960c348946b1dfb00d74a228d0cff
Harald Lapp <harald.lapp@gmail.com>
2012-01-14 02:18:11 +0100

    added method to create UUIDs

--- libsjs/oui/oui.js
+++ libsjs/oui/oui.js
@@ -113,5 +113,24 @@
 
         return _build(tag, def);
     }
+
+    /**
+     * Create a "UUID".
+     *
+     * @octdoc      oui/getUniqId
+     * @see         http://stackoverflow.com/a/105074/85582
+     * @return      string                              The generated Id.
+     */
+    oui.getUUID = (function()
+    /**/
+    {
+        var S4 = function() {
+            return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+        }
+        
+        return function() {
+            return (S4() + S4() + '-' + S4() + '-' + S4() + '-' + S4() + '-' + S4() + S4() + S4());
+        }
+    })();
 })(window);
 
