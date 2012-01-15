80bd9a3b03073310f616fa0d21038a4078007e2c
Harald Lapp <harald.lapp@gmail.com>
2012-01-15 15:09:30 +0100

    started work on button widget

---
+++ libsjs/oui/ui/widget/button.js
@@ -0,0 +1,51 @@
+/**
+ * Button widget.
+ *
+ * @octdoc      widget/button
+ * @copyright   copyright (c) 2012 by Harald Lapp
+ * @author      Harald Lapp <harald@octris.org>
+ */
+/**/
+
+;(function() {
+    if ('button' in oui) return;
+
+    /**
+     * Constructor.
+     *
+     * @octdoc      button/
+     * @public
+     * @param       string      name            Name to set for widget.
+     * @param       object      options         Optional options for widget.
+     * @return      oui.button                  Widget instance.
+     */
+    oui.button = function(name, options)
+    /**/
+    {
+        oui.widget.call(this, name, options);
+    }
+
+    oui.button.prototype = new oui.widget();
+
+    oui.widget.register('button', oui.button);
+
+    /**
+     * Container type of a button.
+     *
+     * @octdoc      widget/container
+     * @public
+     * @var         string
+     */
+    oui.button.prototype.container = 'BUTTON';
+    /**/
+
+    /**
+     * CSS class of a button.
+     *
+     * @octdoc      button/cssclass
+     * @public
+     * @var         string
+     */
+    oui.button.prototype.cssclass = 'oui_button';
+    /**/
+})();
