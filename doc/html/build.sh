cdc7f9b4c179274661b7c3421f18372fc80ec1c4
Harald Lapp <harald.lapp@gmail.com>
2012-01-24 01:26:46 +0100

    initial commit build script for documentation

---
+++ doc/html/build.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+
+#
+# Build script for html documentation.
+#
+
+set -e
+
+cd `dirname $0`
+
+rm -rf dst/*
+
+for src in `find src/* -type f -name "*.html"`; do
+    dst=${src/#src/dst}
+    dir=`dirname $dst`
+    type=`basename $src .html`
+    incl=false
+
+    mkdir -p $dir
+
+    echo "<html>" >> $dst
+    echo "    <head>" >> $dst
+
+    while read row; do
+        if [[ "$row" =~ [a-z0-9]{1,}: ]]; then
+            if [ "$row" = "default:" ]; then
+                incl=true
+
+                continue
+            elif [ "$row" = "$type:" ]; then
+                incl=true
+
+                continue
+            else
+                incl=false
+            fi
+        fi
+
+        if [ $incl = true ]; then
+            row=${row/#- /}
+
+            if [[ "$row" = libsjs/* ]]; then
+                echo "        <script type=\"text/javascript\" src=\"../../$row\"></script>" >> $dst
+            elif [[ "$row" = styles/* ]]; then
+                echo "        <link rel=\"stylesheet\" type=\"text/css\" href=\"../../$row\" />" >> $dst
+            fi
+        fi
+    done < ../../etc/depend.yml
+
+    echo "    </head>" >> $dst
+    echo "    <body>" >> $dst
+    echo "        <div id=\"dialog\"></div>" >> $dst
+
+    cat $src >> $dst
+
+    echo "    </body>" >> $dst
+    echo "</html>" >> $dst
+done
+
