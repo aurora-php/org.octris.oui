1191d521b5180e20617fc71f4f297dea6150468c
Harald Lapp <harald.lapp@gmail.com>
2012-06-13 18:30:10 +0200

    added proxy method

--- libsjs/oui.js
+++ libsjs/oui.js
@@ -33,5 +33,50 @@
             return (S4() + S4() + '-' + S4() + '-' + S4() + '-' + S4() + '-' + S4() + S4() + S4());
         }
     })();
-})(window);
 
+    /**
+     * Builds a proxy for a callback, see example for details.
+     * 
+     * @octdoc  m:oui/proxy
+     * @param   callback                cb          Callback to create proxy for.
+     * @return  callback                            Created proxy callback.
+     * @example
+     * 
+     *          var t1 = proxy(function() {
+     *              return new function() {
+     *                  console.log('t1 build');
+     *                  this.test = function() {
+     *                      console.log('t1 test');
+     *                  }
+     *              };
+     *          });
+     *          var t2 = proxy(function() {
+     *              return new function() {
+     *                  console.log('t2 build');
+     *                  this.test = function() {
+     *                      console.log('t2 test');
+     *                  }
+     *              };
+     *          });
+     *
+     *          t1().test(); t1().test();
+     *          t2().test(); t2().test();
+     */
+    oui.proxy = function(cb)
+    /**/
+    {
+        return (function() {
+            var storage = null;
+
+            return function() {
+                if (storage === null) {
+                    var args = [].splice.call(arguments, 0);        // hack, because 'arguments' is not a _real_ javascript array
+
+                    storage = cb.apply(window, args);
+                }
+
+                return storage;
+            }     
+        })();
+    }
+})(window);
