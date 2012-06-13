f8f4f880b4f7d6b1e6b28219035411c1e9cd8c3f
Harald Lapp <harald.lapp@gmail.com>
2012-06-13 18:37:28 +0200

    started work on oui builder -- oui xml to javascript generator

---
+++ libs/util/ouibuilder/ui.class.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * This file is part of the 'org.octris.oui' package.
+ *
+ * (c) Harald Lapp <harald@octris.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace org\octris\oui\util {
+    /**
+     * Build javascript files from an OUI xml description file.
+     *
+     * @octdoc      c:util/ouibuilder
+     * @copyright   copyright (c) 2012 by Harald Lapp     
+     * @author      Harald Lapp <harald@octris.org>
+     */
+    class ouibuilder
+    /**/
+    {
+        /**
+         * Constructor.
+         *
+         * @octdoc  m:ouibuilder/__construct
+         */
+        public function __construct()
+        /**/
+        {
+        }
+
