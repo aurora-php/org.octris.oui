e42bdc151362358d42b61a088b983c29e0768085
Harald Lapp <harald.lapp@gmail.com>
2012-06-16 10:43:21 +0200

    use determined absolute path

--- test/xsl/test.sh
+++ test/xsl/test.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-xmllint --noblanks ../../doc/oui.xml | xsltproc ../../etc/oui.xsl - | php -r "require_once(__DIR__ . '/../../libs/vendor/jsbeautifier.class.php'); \$js = file_get_contents('php://stdin'); \$opts = new BeautifierOptions(); \$opts->indent_size = 4; print js_beautify(\$js, \$opts);"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-echo ""
-
+xmllint --noblanks $DIR/../../doc/oui.xml | xsltproc $DIR/../../etc/oui.xsl - | php -r "require_once('$DIR/../../libs/vendor/jsbeautifier.class.php'); \$js = file_get_contents('php://stdin'); \$opts = new BeautifierOptions(); \$opts->indent_size = 4; print js_beautify(\$js, \$opts);"
