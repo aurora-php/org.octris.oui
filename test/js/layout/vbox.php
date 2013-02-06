7dfad61806ee72ce05fecb6b81160e0ed682ead2
Harald Lapp <harald.lapp@gmail.com>
2013-02-06 09:08:12 +0100

    added tests for layouts

---
+++ test/js/layout/vbox.php
@@ -0,0 +1,17 @@
+<script id="example" type="text/javascript">
+var d = new oui.dialog();
+d.attach(oui.$('#dialog'), {
+    'children': [
+        {'vbox': {
+            'children': [
+                {'#html': 'row #1'}
+            ]
+        }}
+    ]
+});
+</script>
+<script id="def">
+var args = {
+    
+}
+</script>
