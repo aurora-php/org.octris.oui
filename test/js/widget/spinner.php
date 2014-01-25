641a470c0ec23b562f5631ed8f7272d6ba0a2134
Harald Lapp <harald@octris.org>
2014-01-25 23:20:43 +0100

    fixed test for spinner

--- test/js/widget/spinner.php
+++ test/js/widget/spinner.php
@@ -1,40 +1,43 @@
 <script type="text/javascript">
-oui.$('#dialog').text('Lorem ipsum dolor sit amet, consectetur adipiscing elit. In molestie varius dui,
-ut tincidunt nulla auctor feugiat. Phasellus odio nulla, cursus vitae volutpat a, imperdiet eget elit. Integer
-convallis rhoncus velit, ac aliquet magna hendrerit semper. Fusce faucibus varius turpis ac euismod. Sed consequat
-tellus id est consequat scelerisque. Sed vitae tellus et lectus bibendum convallis pharetra vitae dolor. Nullam
-egestas adipiscing pulvinar. Nullam ultrices, massa eu laoreet facilisis, ante orci volutpat ante, et aliquet nisl
-enim ut dolor. Curabitur auctor aliquam nunc, eleifend adipiscing augue interdum eget. Quisque in nisi blandit odio
-venenatis ultricies sit amet eget arcu. Duis consequat consequat rhoncus. Proin non erat eu enim laoreet lobortis ac
-ut odio. Integer sit amet nulla vitae mauris molestie luctus. Morbi elementum augue at enim tincidunt dapibus.
-
-Vestibulum vel dolor non nunc commodo commodo. Sed gravida est et est ornare sit amet suscipit est venenatis.
-Integer semper lorem iaculis tellus bibendum luctus. Integer at aliquet lorem. Curabitur hendrerit, elit vel
-bibendum vestibulum, massa odio ornare sem, ut tincidunt tellus mauris eu mi. Integer iaculis, felis tincidunt pretium
-pulvinar, mi tellus dapibus sapien, ac posuere arcu velit vel purus. Mauris lobortis gravida tellus, at congue erat
-posuere et. Proin mollis tempus sodales. Sed ut mauris et ante molestie euismod. Nam congue metus nec neque cursus
-sodales. Praesent ullamcorper lobortis porta. In quis urna leo. Vestibulum erat leo, interdum quis interdum id,
-consectetur nec ante. Nunc urna enim, aliquet vel malesuada non, auctor non dolor.
-
-Praesent vel eros neque, sit amet dictum ligula. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per
-inceptos himenaeos. Fusce accumsan magna et nunc congue rutrum. Quisque tempor cursus diam ut pulvinar. Maecenas adipiscing
-commodo neque at euismod. Nulla quis felis ligula. Nunc tempus mollis pharetra. Suspendisse dictum nisl nec metus tempus nec
-pretium velit sodales. Nullam eu lorem urna, vitae tincidunt lacus. Sed imperdiet dui nec nulla facilisis at elementum massa
-lacinia. Praesent vel fermentum metus.
-
-Curabitur tellus arcu, vehicula accumsan pulvinar quis, semper sed lacus. Nulla eu nisi a eros auctor dictum et quis ipsum.
-Vivamus vel venenatis est. Nunc ligula lacus, sodales a ultricies ut, mattis ac leo. Nulla vel lectus nec nulla mollis
-consectetur. Sed a consequat mauris. Curabitur eu libero nulla, in tincidunt orci. Mauris nisi erat, sollicitudin quis
-ultricies ac, eleifend nec arcu. Ut ultrices nulla in sapien luctus sit amet elementum dolor sollicitudin. Donec commodo
-consectetur urna ac hendrerit.
-
-Vestibulum gravida dui eget justo tincidunt varius. Nam a lorem eu ante sagittis gravida. Aenean urna quam, venenatis in
-sodales non, imperdiet sit amet purus. Aliquam vel ullamcorper velit. Vestibulum rutrum venenatis vestibulum. Quisque
-imperdiet cursus mattis. Maecenas ante orci, scelerisque sed congue in, luctus id tortor. Phasellus quis odio nec mauris
-tristique laoreet eu sit amet nisi. Nam condimentum metus ut massa bibendum mollis. Quisque quis tellus nec odio aliquam
+oui.$('#dialog').text('Lorem ipsum dolor sit amet, consectetur adipiscing elit. In molestie varius dui, \
+ut tincidunt nulla auctor feugiat. Phasellus odio nulla, cursus vitae volutpat a, imperdiet eget elit. Integer \
+convallis rhoncus velit, ac aliquet magna hendrerit semper. Fusce faucibus varius turpis ac euismod. Sed consequat \
+tellus id est consequat scelerisque. Sed vitae tellus et lectus bibendum convallis pharetra vitae dolor. Nullam \
+egestas adipiscing pulvinar. Nullam ultrices, massa eu laoreet facilisis, ante orci volutpat ante, et aliquet nisl \
+enim ut dolor. Curabitur auctor aliquam nunc, eleifend adipiscing augue interdum eget. Quisque in nisi blandit odio \
+venenatis ultricies sit amet eget arcu. Duis consequat consequat rhoncus. Proin non erat eu enim laoreet lobortis ac \
+ut odio. Integer sit amet nulla vitae mauris molestie luctus. Morbi elementum augue at enim tincidunt dapibus. \
+ \
+Vestibulum vel dolor non nunc commodo commodo. Sed gravida est et est ornare sit amet suscipit est venenatis. \
+Integer semper lorem iaculis tellus bibendum luctus. Integer at aliquet lorem. Curabitur hendrerit, elit vel \
+bibendum vestibulum, massa odio ornare sem, ut tincidunt tellus mauris eu mi. Integer iaculis, felis tincidunt pretium \
+pulvinar, mi tellus dapibus sapien, ac posuere arcu velit vel purus. Mauris lobortis gravida tellus, at congue erat \
+posuere et. Proin mollis tempus sodales. Sed ut mauris et ante molestie euismod. Nam congue metus nec neque cursus \
+sodales. Praesent ullamcorper lobortis porta. In quis urna leo. Vestibulum erat leo, interdum quis interdum id, \
+consectetur nec ante. Nunc urna enim, aliquet vel malesuada non, auctor non dolor. \
+ \
+Praesent vel eros neque, sit amet dictum ligula. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per \
+inceptos himenaeos. Fusce accumsan magna et nunc congue rutrum. Quisque tempor cursus diam ut pulvinar. Maecenas adipiscing \
+commodo neque at euismod. Nulla quis felis ligula. Nunc tempus mollis pharetra. Suspendisse dictum nisl nec metus tempus nec \
+pretium velit sodales. Nullam eu lorem urna, vitae tincidunt lacus. Sed imperdiet dui nec nulla facilisis at elementum massa \
+lacinia. Praesent vel fermentum metus. \
+ \
+Curabitur tellus arcu, vehicula accumsan pulvinar quis, semper sed lacus. Nulla eu nisi a eros auctor dictum et quis ipsum. \
+Vivamus vel venenatis est. Nunc ligula lacus, sodales a ultricies ut, mattis ac leo. Nulla vel lectus nec nulla mollis \
+consectetur. Sed a consequat mauris. Curabitur eu libero nulla, in tincidunt orci. Mauris nisi erat, sollicitudin quis \
+ultricies ac, eleifend nec arcu. Ut ultrices nulla in sapien luctus sit amet elementum dolor sollicitudin. Donec commodo \
+consectetur urna ac hendrerit. \
+ \
+Vestibulum gravida dui eget justo tincidunt varius. Nam a lorem eu ante sagittis gravida. Aenean urna quam, venenatis in \
+sodales non, imperdiet sit amet purus. Aliquam vel ullamcorper velit. Vestibulum rutrum venenatis vestibulum. Quisque \
+imperdiet cursus mattis. Maecenas ante orci, scelerisque sed congue in, luctus id tortor. Phasellus quis odio nec mauris \
+tristique laoreet eu sit amet nisi. Nam condimentum metus ut massa bibendum mollis. Quisque quis tellus nec odio aliquam \
 vulputate eu a massa. Donec vel nisi non mi interdum scelerisque quis at felis. Suspendisse potenti.');
 </script>
 <script id="example" type="text/javascript">
+var spinner = new oui.widget.spinner();
+spinner.attach(oui.$('#dialog'), {});
+spinner.show();
 </script>
 <script id="def">
 var args = {
